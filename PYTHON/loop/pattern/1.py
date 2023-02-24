dcfc39812172241ea1bf91b3b3cabf523c38b6b8
pratham40 <122166099+pratham40@users.noreply.github.com>
2023-02-24 23:04:17 +0530

    Add files via upload

---
+++ PYTHON/loop/pattern/1.py
@@ -0,0 +1,18 @@
+n=6
+for i in range(1,6):
+    n-=1
+    for j in range(n,0,-1):
+        print("*",end="   ")
+    print()
+    
+#o/p
+
+'''
+
+*   *   *   *   *   
+*   *   *   *   
+*   *   *
+*   *
+*
+
+'''
