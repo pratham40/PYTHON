bed98a72a0a5c9e0fc74a3c1dcce2ade9ebcaade
pratham40 <122166099+pratham40@users.noreply.github.com>
2023-02-24 23:27:47 +0530

    Add files via upload

---
+++ PYTHON/loop/pattern/3.py
@@ -0,0 +1,19 @@
+num=0
+for i in range(1,6):
+    num+=1
+    for j in range(1,num+1):
+        print(j,end=" ")
+    print()        
+    
+    
+# o/p
+
+'''
+
+1 
+1 2 
+1 2 3
+1 2 3 4
+1 2 3 4 5
+
+'''
