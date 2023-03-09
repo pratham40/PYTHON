708849e44797e7e805d41ecaa3c15d3ab723c929
pratham40 <122166099+pratham40@users.noreply.github.com>
2023-03-09 21:46:20 +0530

    Add files via upload

---
+++ PYTHON/loop/sumasper.py
@@ -0,0 +1,13 @@
+sum1=sum2=0
+n1=int(input("enter no1"))
+n2=int(input("enter no1"))
+n3=int(input("enter no1"))
+sum1=n1+n2+n3
+if n1!=n2 and n1!=n3:
+    sum2+n1
+if n2!=n1 and n2!=n3:
+    sum2+n2
+if n3!=n1 and n3!=n2:
+    sum2+n3
+print(sum1)
+print(sum2)
